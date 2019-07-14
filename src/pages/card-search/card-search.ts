5d2cc2e2a7c796b80ead888d59b753e6a90c3954
Pedro de Souza e Silva <pedrosouzaesilva@id.uff.br>
2019-07-13 23:59:45 -0300

    Search com color identity

--- src/pages/card-search/card-search.ts
+++ src/pages/card-search/card-search.ts
@@ -14,9 +14,27 @@ import { CardProvider } from '../../providers/card/card';
   templateUrl: 'card-search.html',
 })
 export class CardSearchPage {
-  colors = ["white", "blue", "black", "red", "green", "colorless"];
+  parameters = {
+    colors: {},
+  };
+  colors = [
+            {name: "white", letter: "W", value: false},
+            {name: "blue", letter: "U", value: false},
+            {name: "black", letter: "B", value: false},
+            {name: "red", letter: "R", value: false},
+            {name: "green", letter: "G", value: false},
+          ];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public cardProvider: CardProvider) {
+  }
+
+  cardSearch(parameters){
+    let colorParameter = [];
+    for(let color of this.colors) {
+      if(color.value) colorParameter.push(color.letter);
+    }
+    parameters.colors = colorParameter;
+    this.cardProvider.cardSearch(parameters);
   }
 
   ionViewDidLoad() {
--- src/providers/card/card.ts
+++ src/providers/card/card.ts
@@ -18,20 +18,30 @@ export class CardProvider {
   }
 
   getMTGJson() {
-    this.http.get('https://mtgjson.com/json/AllCards.json').subscribe(data => {
-      //let map = new Map(data);
-      this.storage.set('allCards', data).then(result => {
+    if(! this.storage.get('allCards')) {
+      this.http.get('https://mtgjson.com/json/AllCards.json').subscribe(data => {
+        this.storage.set('allCards', data).then(result => {
+        });
       });
-    });
+    }
+    else {
+      console.log("JSON já armazenado");
+    }
   }
 
   public cardSearch(parameters) {
     let results = [];
+    console.log(parameters);
     this.storage.get('allCards').then(cards => {
       for(let card in cards){
         let aux = cards[card];
         
-        if(aux.type.includes(parameters.type)){
+        if(
+          this.checkCardName(aux, parameters.name) &&
+          this.checkCardType(aux, parameters.type) &&
+          this.checkCardText(aux, parameters.text) &&
+          this.checkCardColors(aux, parameters.colors)
+        ){
           results.push(aux);
         }
       }
@@ -41,6 +51,31 @@ export class CardProvider {
   }
 
   private checkCardName(card, name) {
-    return card.name == name;
+    if(name == undefined) name = '';
+    return card.name.match(new RegExp(name, "i"));
+  }
+
+  private checkCardText(card, text) {
+    if(text == undefined) text = [];
+    else text = text.split(' ');
+    for(let sentence of text){
+      if(!card.text || !card.text.match(new RegExp(sentence, "i"))){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private checkCardType(card, type) {
+    if(type == undefined) type = '';
+    return card.type.match(new RegExp(type, "i"));
+  }
+
+  private checkCardColors(card, colors) {
+    for(let color of colors){
+      if(!card.colorIdentity) console.log(card.colorIdentity);
+      if(!card.colorIdentity.includes(color)) return false;
+    }
+    return true;
   }
 }
